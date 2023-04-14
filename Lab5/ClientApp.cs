e0dcce68833e4060412a9e36871ac1192c9bf767
Dmitriy <Qusild@outlook.com>
2023-04-14 17:28:29 +0300

    Changed size of Double windows

--- Lab5/ClientApp.cs
+++ Lab5/ClientApp.cs
@@ -27,66 +27,6 @@ namespace OOPLab5
             //sendMsg("Init", size, matrix);
             setMatrixView();
         }
-        /*async Task sendMsg(string commandLine, int size, List<List<(int, int)>> matrix)
-        {
-            StringBuilder tmp = new StringBuilder();
-            tmp.Append(commandLine);
-            tmp.Append("\n");
-            tmp.Append(size.ToString());
-            tmp.Append("\n");
-            foreach(var row in matrix)
-            {
-                foreach ((int, int) elem in row)
-                {
-                    tmp.Append(elem.Item1.ToString());
-                    tmp.Append(' ');
-                    tmp.Append(elem.Item2.ToString());
-                    tmp.Append(' ');
-                }
-                    
-            }
-            string message = tmp.ToString();
-            using (var tcpClient = new TcpClient())
-            {
-                await tcpClient.ConnectAsync("127.0.0.1", 1005);
-                using (var networkStream = tcpClient.GetStream())
-                using (var writer = new StreamWriter(networkStream))
-                using (var reader = new StreamReader(networkStream))
-                {
-                    //while (true)
-                    //{
-                    writer.AutoFlush = true;
-                    networkStream.ReadTimeout = 5000;
-                    char[] buffer = new char[512];
-                    //Console.WriteLine("Write Message");;
-                    //bufferedStream.WriteAsync(byteArray, 0, byteArray.Length);
-                    await writer.WriteAsync(message);
-                    //bufferedStream.Flush();
-                    //}
-                    await reader.ReadAsync(buffer, 0, buffer.Length);
-                    string ansBuffered = new string(buffer);
-                    //bufferedStream.ReadAsync(buffer, 0, buffer.Length);
-                    string[] values = ansBuffered.Split('\n');
-                    this.size = Convert.ToInt32(values[0]);
-                    this.rank = Convert.ToInt32(values[2]);
-                    this.determinant = (Convert.ToInt32(values[3].Split(' ')[0]), Convert.ToInt32(values[3].Split(' ')[1]));
-                    string[] strTmp = values[1].Split(' ');
-                    List<List<(int, int)>> tmpList = new List<List<(int, int)>>();
-                    for (int i = 0; i < size; i++)
-                    {
-                        tmpList.Add(new List<(int, int)>());
-                        for (int j = 0; j < size; j++)
-                        {
-                            tmpList[i].Add((Convert.ToInt32(strTmp[i * size * 2 + j * 2]),
-                                         Convert.ToInt32(strTmp[i * size * 2 + j * 2 + 1])));
-                        }
-
-                    }
-                    this.matrix = tmpList;
-                }
-                tcpClient.Close();
-            }
-        }*/
         public void ExitButton_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -115,7 +55,7 @@ namespace OOPLab5
                     sizeX = 140; sizeY = 40;
                     break;
                 case "Double":
-                    sizeX = 60; sizeY = 40;
+                    sizeX = 50; sizeY = 40;
                     break;
             }
             LayoutBox.SetBounds(12, 12, matrix.Count() * sizeX, matrix.Count() * sizeY);
