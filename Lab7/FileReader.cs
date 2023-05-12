19d62cd980d227ba35c561d9027ea7b81c968493
Dmitriy <Qusild@outlook.com>
2023-05-12 19:19:20 +0300

    Added lab7

---
+++ Lab7/FileReader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+namespace OOPLab7
+{
+    public class FileReader
+    {
+        public FileReader() { }
+        public List<List<int>> readMatrix(string filename) 
+        {
+            List<string> matrix = File.ReadAllLines(filename).ToList();
+            List<List<int>> connectionMatrix = new List<List<int>>();
+            if (matrix[0][0] != 'I')
+            {
+                MessageBox.Show("ERROR: Wrong Matrix File");
+                return null;
+            }
+            for (int i = 1; i < matrix.Count; i++)
+            {
+                String[] row = matrix[i].Split(' ');
+                if (row[0] != matrix[0].Split(' ')[i])
+                {
+                    MessageBox.Show("ERROR: Wrong Matrix File");
+                    return null;
+                }                    
+                if (row.Count()!= matrix.Count)
+                {
+                    MessageBox.Show("ERROR: Wrong Matrix File");
+                    return null;
+                }
+                connectionMatrix.Add(new List<int>());
+                for (int j = 1; j < row.Count();j++)
+                {
+                    int tmp = Convert.ToInt32(row[j]);
+                    if ((tmp != 0) && (tmp != 1))
+                    {
+                        MessageBox.Show("ERROR: Wrong Matrix File");
+                        return null;
+                    }
+                    connectionMatrix[i-1].Add(tmp);
+                }
+            }
+            MessageBox.Show("Matrix Read Successfully");
+            return connectionMatrix;
+        }
+
+    }
+}
---
+++ Lab7/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOPLab7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GraphButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "TextFiles|*.txt";
+            Nullable<bool> dialogCompleted = ofd.ShowDialog();
+            if (dialogCompleted == true) 
+            {
+                String fileName = ofd.FileName;
+                FileReader fr = new FileReader();
+                GraphVisualizer gv = new GraphVisualizer(fr.readMatrix(fileName),ref graphCanvas);
+            }
+        }
+    }
+}
